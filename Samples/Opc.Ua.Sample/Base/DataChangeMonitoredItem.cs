3dd761aebe6fa4ae3be07163cf6e1b7f64df61d9
Marc Schier <marcschier@hotmail.com>
2025-10-05 10:39:42 +0200

    Update

--- Samples/Opc.Ua.Sample/Base/DataChangeMonitoredItem.cs
+++ Samples/Opc.Ua.Sample/Base/DataChangeMonitoredItem.cs
@@ -2,7 +2,7 @@
  * Copyright (c) 2005-2019 The OPC Foundation, Inc. All rights reserved.
  *
  * OPC Foundation MIT License 1.00
- * 
+ *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without
@@ -11,7 +11,7 @@
  * copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following
  * conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
@@ -30,6 +30,8 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Opc.Ua.Server;
 
 namespace Opc.Ua.Sample
@@ -252,7 +254,7 @@ namespace Opc.Ua.Sample
 
                 m_samplingInterval = samplingInterval;
 
-                // calculate the next sampling interval.                
+                // calculate the next sampling interval.
                 long newSamplingInterval = (long)(m_samplingInterval * TimeSpan.TicksPerMillisecond);
 
                 if (m_samplingInterval > 0)
@@ -850,7 +852,12 @@ namespace Opc.Ua.Sample
             diagnostics.Enqueue(diagnosticInfo);
         }
 
-        public bool Publish(OperationContext context, Queue<MonitoredItemNotification> notifications, Queue<DiagnosticInfo> diagnostics, uint maxNotificationsPerPublish)
+        public bool Publish(
+            OperationContext context,
+            Queue<MonitoredItemNotification> notifications,
+            Queue<DiagnosticInfo> diagnostics,
+            uint maxNotificationsPerPublish,
+            ILogger logger)
         {
             return Publish(context, notifications, diagnostics);
         }
